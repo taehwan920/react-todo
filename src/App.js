947bd4e89f3cabc23246f60daa6a54ebfee679b1
Yang Taehwan <taehwan920@gmail.com>
2020-03-31 12:16:03 +0900

    Make React Web Page works!
    홈페이지가 작동을하지 않아서 알아보니 클래스를 직접 JSX안에 넣으면 안된다는걸 알 수 있었다.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      class YourName extends React.Component {
-        render() {
-          return (
+class YourName extends React.Component {
+    render() {
+        return (
             < header className="headerForName" >
-              <form action="/" className="yourName" autoComplete="none">
-                <label>
-                  <input type="text" placeholder="Your Name?" />
-                </label>
-              </form >
+                <p>ssdflj</p>
+                <form action="/" className="yourName" autoComplete="none">
+                    <label>
+                        <input type="text" placeholder="Your Name?" />
+                    </label>
+                </form >
             </header >
-          );
-        };
-      },
-      class WriteToDo extends React.Component {
-        render() {
-          return (
-            <center className="centerForWriting">
-              <form action="/" className="WriteToDo">
-                <label>
-                  <input type="text" placeholder="What are you going to do today?" />
-                </label>
-              </form>
-            </center>
-          )
-        };
-      }
-    );
-  };
+        )
+    }
+}
 
-  //이름 입력할 form
-  //input
-  //todo입력할 form 
-  //input
-  //이름이 입력됐을 때 인사 문구 출력하는 영역
-  //todo가 입력됐을 떄 todo 리스트 출력하는 영역(RUD 구현)
-};
+class WriteToDo extends React.Component {
+    render() {
+        return (
+            <center className="centerForWriting">
+                <form action="/" className="WriteToDo">
+                    <label>
+                        <input type="text" placeholder="What are you going to do today?" />
+                    </label>
+                </form>
+            </center>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <YourName></YourName>
+                <WriteToDo></WriteToDo>
+            </div>
+        )
+    }
+}
+
+//이름 입력할 form
+//input
+//todo입력할 form 
+//input
+//이름이 입력됐을 때 인사 문구 출력하는 영역
+//todo가 입력됐을 떄 todo 리스트 출력하는 영역(RUD 구현)
+
 
 export default App;
