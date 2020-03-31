4ca482cddbfde5f4d71f307d60858c1f657f176b
Yang Taehwan <taehwan920@gmail.com>
2020-03-31 15:16:41 +0900

    complete name input part
    App컴포넌트에 메소드를 추가하고 YourName에 입력한 이름 값을 가지고와서 쓸 수 있도록 했다.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,78 @@
 import React from 'react';
 
-class YourName extends React.Component {
-    render() {
-        return (
-            < header className="headerForName" >
-                <p>ssdflj</p>
-                <form action="/" className="yourName" autoComplete="none">
-                    <label>
-                        <input type="text" placeholder="Your Name?" />
-                    </label>
-                </form >
-            </header >
-        )
-    }
-}
+
 
 class WriteToDo extends React.Component {
-    render() {
-        return (
-            <center className="centerForWriting">
-                <form action="/" className="WriteToDo">
-                    <label>
-                        <input type="text" placeholder="What are you going to do today?" />
-                    </label>
-                </form>
-            </center>
-        )
-    }
+  render() {
+    return (
+      <center className="centerForWriting">
+        <form action="" className="WriteToDo">
+          <label>
+            What To Do!
+            <input
+              type="text"
+              placeholder="What are you going to do today?"
+            />
+          </label>
+        </form>
+      </center>
+    )
+  }
+}
+
+class HelloName extends React.Component {
+  render() {
+    return (
+      <header className="helloName">
+        Hello! {this.props.name}
+      </header>
+    )
+  }
+}
+
+class YourName extends React.Component {
+  render() {
+    return (
+      <header className="headerForName">
+        <form action="/" method="post" className="yourName" autoComplete="none"
+          onSubmit={function (e) {
+            e.preventDefault();
+            this.props.onSubmit(e.target.name.value);
+          }.bind(this)}>
+          <label>
+            Name!
+            <input type="text" placeholder="Your Name?" name="name" />
+          </label>
+        </form>
+      </header >
+    )
+  }
 }
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <YourName></YourName>
-                <WriteToDo></WriteToDo>
-            </div>
-        )
-    }
+  state = {
+    userName: ""
+  }
+
+  isRegistered() {
+    if (this.state.userName) {
+      return <HelloName name={this.state.userName}></HelloName>
+    };
+    return <YourName onSubmit={
+      function (str_name) {
+        this.setState({ userName: str_name })
+      }.bind(this)
+    }></YourName >
+  }
+
+  render() {
+    return (
+      <main className="App">
+        {this.isRegistered()}
+        <WriteToDo></WriteToDo>
+      </main>
+    )
+  }
 }
 
 //이름 입력할 form
