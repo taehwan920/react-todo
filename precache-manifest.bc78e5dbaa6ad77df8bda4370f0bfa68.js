ca4f598cca480c81b5b8eea5ed65e0b78253cea1
Yang Taehwan <taehwan920@gmail.com>
2020-04-03 22:11:55 +0900

    Updates

---
+++ precache-manifest.bc78e5dbaa6ad77df8bda4370f0bfa68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55268c0d4f61dbb2c2580a2fb530edd8",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "00733e07ce2ca43de64d",
+    "url": "/react-todo/static/css/main.41134983.chunk.css"
+  },
+  {
+    "revision": "40f3934769e8af7e32e8",
+    "url": "/react-todo/static/js/2.e71b42ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.e71b42ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00733e07ce2ca43de64d",
+    "url": "/react-todo/static/js/main.1be18852.chunk.js"
+  },
+  {
+    "revision": "52cb9f894243c1334941",
+    "url": "/react-todo/static/js/runtime-main.bb662e3c.js"
+  },
+  {
+    "revision": "4fcea8aa23ab911d62a9d070c52afaa0",
+    "url": "/react-todo/static/media/sheep1600.4fcea8aa.jpg"
+  },
+  {
+    "revision": "a701a2f775531775aa4fc41a3b0f49c3",
+    "url": "/react-todo/static/media/sheepphone.a701a2f7.jpg"
+  }
+]);
